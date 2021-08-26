6421c345cd60056f2c203547ba44d4ccb61f28de
George Usynin <angiejo03@gmail.com>
2021-08-26 17:49:51 +0300

    add redirect to loginPage if user is not authorized

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {AppRootStateType} from '../../bll/store';
+import {Redirect} from 'react-router-dom';
+import {PATH} from '../../App';
 
 export const Profile = () => {
+
+    const isAuth = useSelector<AppRootStateType, boolean>(state => state.auth.isAuth)
+
+    if (!isAuth) {
+        return <Redirect to={PATH.LOGIN}/>
+    }
+
     return (
         <div>
             PROFILE
